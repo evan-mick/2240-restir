3787ecb3a43b6b17ff0783925c8aa7ef654eb26c
evan <evsammick@gmail.com>
2025-04-28 02:54:41 -0400

    big things happening

--- src/shaders/tile.glsl
+++ src/shaders/tile.glsl
@@ -84,7 +84,7 @@ void main(void)
 
     //vec4 pixelColor = PathTrace(ray);
 
-    color = pixelColor * W + accumColor;
+    color = pixelColor + accumColor;
 
     reservoirOut0 = texelFetch(reservoirs0, ivec2(gl_FragCoord.xy), 0);
     reservoirOut1 = texelFetch(reservoirs1, ivec2(gl_FragCoord.xy), 0);
